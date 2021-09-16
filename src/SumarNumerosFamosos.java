fe2c6ab81cdabf13afe5f2992aa8cc4f3cf2d1bc
Sergio <sergon149@alu.centroafuera.es>
2021-09-16 11:05:09 +0200

    guardar ejemplo

--- src/SumarNumerosFamosos.java
+++ src/SumarNumerosFamosos.java
@@ -3,7 +3,7 @@ public class SumarNumerosFamosos {
     private final float valorPi = 3.1416f;
     private final float valorE = 2.71828f;
 
-    private float sumarNumero(int numero1, float numeroFamoso){
+    private float sumarNumero(int numero1, float numeroFamoso) {
         return numero1 + numeroFamoso;
     }
     public float sumarPi(int numero){
